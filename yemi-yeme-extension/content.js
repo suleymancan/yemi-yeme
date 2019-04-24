1a0b6610365d1484ab6f716be54f8e8c1659bc41
Suleyman Can <27suleymancan@gmail.com>
2019-04-24 19:00:06 +0300

    at clickbait event focused on twitter

--- yemi-yeme-extension/content.js
+++ yemi-yeme-extension/content.js
@@ -1,18 +1,48 @@
-console.log('content');
-
-window.addEventListener('mouseup',wordSelected);
 
 
+window.addEventListener('load',wordSelected);
+
+function createChildElement(responseText){
+    let childElement = document.createElement('div');
+    childElement.textContent = responseText;
+
+    if(responseText == 'CLICKBAIT'){
+        childElement.style.color = "red";
+        childElement.style.textAlign = "right";
+    }
+    else{
+        childElement.style.color = "green";
+        childElement.style.textAlign = "right";
+    }
+    return childElement;
+}
 
 function wordSelected(){
+
+
+
     //burada a taginin textlerini otomatik sec.
-    let selectedText = window.getSelection().toString().trim();
-    console.log(selectedText);
-    if(selectedText.length > 0){
-        let message = {
-            text: selectedText
+    let tweetList = document.getElementsByClassName('TweetTextSize  js-tweet-text tweet-text');
+
+    for(let tweet in tweetList){
+        let selectedText = tweetList[tweet].innerText;
+        if(selectedText && selectedText.length > 0){
+            let xhttp = new XMLHttpRequest();
+            let url = "http://localhost:8080/api/yemi-yeme?source="+selectedText;
+
+            xhttp.onreadystatechange = function(){
+
+                if(this.readyState == 4 && this.status == 200){
+                    tweetList[tweet].appendChild(createChildElement(this.responseText));
+                }
+
+            };
+            xhttp.open("GET",url, true);
+            xhttp.send();
+
         }
-        chrome.runtime.sendMessage(message);
     }
+
+
 }
 
