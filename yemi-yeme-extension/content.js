601b1240661a03292f2594819308192b4abc6678
Suleyman Can <27suleymancan@gmail.com>
2019-04-26 14:57:38 +0300

    extension: the query was made only for external links.

--- yemi-yeme-extension/content.js
+++ yemi-yeme-extension/content.js
@@ -1,50 +1,103 @@
+window.addEventListener('load', pageLoad);
 
 
-window.addEventListener('load',wordSelected);
+function pageLoad() {
 
-function createChildElement(responseText){
-    let childElement = document.createElement('div');
-    childElement.textContent = responseText;
+  let tweetList = document.getElementsByClassName('TweetTextSize  js-tweet-text tweet-text');
 
-    if(responseText == 'CLICKBAIT'){
-        childElement.style.color = "red";
-        childElement.style.textAlign = "right";
+  let filterTweetList = doFilterTweetList(tweetList);
+
+  for (let tweet in filterTweetList) {
+
+    let selectedText = filterTweetList[tweet].innerText;
+    console.log(selectedText);
+    if (selectedText && selectedText.length > 0) {
+      let xhttp = new XMLHttpRequest();
+      let url = "http://localhost:8080/api/yemi-yeme?source=" + selectedText;
+
+      xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+          filterTweetList[tweet].appendChild(createChildElement(this.responseText));
+        }
+
+      };
+      xhttp.open("GET", url, true);
+      xhttp.send();
+
     }
-    else{
-        childElement.style.color = "green";
-        childElement.style.textAlign = "right";
+  }
+
+
+}
+
+function doFilterTweetList(tweetList) {
+  let filterTweetList = new Array();
+  for (let tweet in tweetList) {
+    if (isTweetLangEqualsTR(tweetList[tweet]) && isTweetChildElementNews(tweetList[tweet])) {
+      filterTweetList[tweet] = tweetList[tweet];
     }
-    return childElement;
+  }
+  return filterTweetList;
+
+}
+
+function isTweetLangEqualsTR(tweet) {
+  if (tweet.lang === 'tr') {
+    return true;
+  }
+  return false;
+}
+
+function isTweetChildElementNews(tweet) {
+  if (tweet.firstElementChild != null) {
+
+    for (let child in tweet.children) {
+
+      if (isChildTweetTagNameEqualsA(tweet.children[child]) && isChildTweetHasAttributeAndAttributeNews(tweet.children[child])) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 
-
-function wordSelected(){
-
-    let tweetList = document.getElementsByClassName('TweetTextSize  js-tweet-text tweet-text');
-
-    for(let tweet in tweetList){
-        if(tweetList[tweet].lang === 'tr' && tweetList[tweet].firstElementChild!=null && tweetList[tweet].firstElementChild.tagName === 'A' && tweetList[tweet].firstElementChild.hasAttribute('data-expanded-url')) {
-            let selectedText = tweetList[tweet].innerText;
-            console.log(selectedText);
-            if (selectedText && selectedText.length > 0) {
-                let xhttp = new XMLHttpRequest();
-                let url = "http://localhost:8080/api/yemi-yeme?source=" + selectedText;
-
-                xhttp.onreadystatechange = function () {
-
-                    if (this.readyState == 4 && this.status == 200) {
-                        tweetList[tweet].appendChild(createChildElement(this.responseText));
-                    }
-
-                };
-                xhttp.open("GET", url, true);
-                xhttp.send();
-
-            }
-        }
-    }
+function isChildTweetTagNameEqualsA(child) {
+  if (child.tagName === 'A') {
+    return true;
+  }
+  return false;
+}
 
 
+function isChildTweetHasAttributeAndAttributeNews(child) {
+  if (child.hasAttribute('data-expanded-url') && isAttributeNews(child.getAttribute('data-expanded-url'))) {
+    return true;
+  }
+  return false;
+}
+
+
+//check youtube vs.
+function isAttributeNews(attribute) {
+  if (attribute.match('^(http|https):\\/\\/twitter.com\\/.*')) {
+    return false;
+  }
+  return true;
+}
+
+function createChildElement(responseText) {
+  let childElement = document.createElement('div');
+  childElement.textContent = responseText;
+
+  if (responseText == 'CLICKBAIT') {
+    childElement.style.color = "red";
+    childElement.style.textAlign = "right";
+  } else {
+    childElement.style.color = "green";
+    childElement.style.textAlign = "right";
+  }
+  return childElement;
 }
 
