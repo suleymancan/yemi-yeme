2e2a7935b6f1210e2cff9df47f5c7208fb4435de
Suleyman Can <27suleymancan@gmail.com>
2019-05-07 13:57:56 +0300

    handle exception

--- src/main/java/com/suleymancanblog/yemiyeme/clickbait/ClickbaitRestController.java
+++ src/main/java/com/suleymancanblog/yemiyeme/clickbait/ClickbaitRestController.java
@@ -1,6 +1,7 @@
 package com.suleymancanblog.yemiyeme.clickbait;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -11,24 +12,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/yemi-yeme")
 @AllArgsConstructor
+@Slf4j
 public class ClickbaitRestController {
 
 	private final ClickbaitService clickbaitService;
 
 	@CrossOrigin
 	@GetMapping
-	public String test(@RequestParam String source){
+	public String test(@RequestParam String source) {
 		final PrizmaFeature prizmaFeature = clickbaitService.createNewsFeature(source);
-		try {
-			final NewsLabel predict = clickbaitService.predict(prizmaFeature);
-			return predict.getNewsLabel();
-		}
-		catch (Exception e){
-			e.printStackTrace();
-		}
-
-		return "ERROR";
+		final NewsLabel predict = clickbaitService.predict(prizmaFeature);
+		return predict.getNewsLabel();
 	}
 
+	@ExceptionHandler(Exception.class)
+	public void handleException(Exception e) {
+		log.error(e.getMessage(), e);
+	}
 
 }
